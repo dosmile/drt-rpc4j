824a4073b1c780244469ecff830df59604d7ae31
duruitao <duruitao@meituan.com>
2016-06-26 14:32:04 +0800

    [0612_rpc4j]：anotaion

--- rpc4j-sample-server/src/main/java/org/rpc4j/sample/server/RpcBootstrap.java
+++ rpc4j-sample-server/src/main/java/org/rpc4j/sample/server/RpcBootstrap.java
@@ -4,6 +4,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class RpcBootstrap {
 
+    //启动service，并对外发布
+    //由于在spring.xml中配置了zookeeper服务器、RPC服务器，因而当spring启动之后，就能对外发布HelloServiceImpl这个服务了
     public static void main(String[] args) {
         new ClassPathXmlApplicationContext("spring.xml");
     }
--- rpc4j-server/src/main/java/org/rpc4j/server/annotation/RPCService.java
+++ rpc4j-server/src/main/java/org/rpc4j/server/annotation/RPCService.java
@@ -9,9 +9,8 @@ import java.lang.annotation.Target;
 
 /**
  * RPC 请求注解（标注在服务实现类上）
- * 
- * @author huangyong
- * @since 1.0.0
+ *
+ * java中元注解有四个： @Retention @Target @Document @Inherited
  */
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
